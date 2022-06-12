31a8a2c5c66d63b5e9f52038c8096bea11b93ed1
Kirill <kirill.barmin@behavox.com>
2022-06-11 20:43:42 -0400

    - Added font Game page - Added initial entities - Added FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { GameComponent } from './pages/game/game.component';
+import { PlayerHandComponent } from './components/player-hand/player-hand.component';
+import { CardComponent } from './components/card/card.component';
+import { DeckComponent } from './components/deck/deck.component';
+import {routes} from './app.route';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
+  declarations: [
+    AppComponent,
+    GameComponent,
+    PlayerHandComponent,
+    CardComponent,
+    DeckComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/player-hand/player-hand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-hand',
+  templateUrl: './player-hand.component.html',
+  styleUrls: ['./player-hand.component.scss']
+})
+export class PlayerHandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
